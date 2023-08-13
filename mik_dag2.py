9f4f692b79b5cbdf298c208df9c8f7db30f8ca36
Michael Kissel <mail@kissel.ch>
2023-08-13 17:02:30 +0200

    init commit DAG's

---
+++ mik_dag2.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def helloWorld():
+    print("Hello World")
+
+with DAG(
+    dag_id="mik_dag1",
+    start_date=datetime(2021,1,1),
+    schedule_interval=None,
+    catchup=False,
+    tags=["load", "bronze"],
+) as dag:
+    task1 = PythonOperator(
+    task_id="hello_world",
+    python_callable=helloWorld)
